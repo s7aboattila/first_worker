35f2be0de668569e30ac975244b24a0db51318ee
s7aboattila <32704500+s7aboattila@users.noreply.github.com>
2025-08-05 21:10:46 +0000

    Add worker

---
+++ github_pages_worker.js
@@ -0,0 +1,15 @@
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+  const url = new URL(request.url)
+
+  // Rewrite "/" to "/my-subdir/"
+  if (url.pathname === "/") {
+    url.pathname = "/my-subdir/"
+    return fetch(url.toString(), request)
+  }
+
+  return fetch(request)
+}
